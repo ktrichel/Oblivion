3a26505598e1028dd59780383e26d0d9c6719f98
davidr201 <33270359+davidr201@users.noreply.github.com>
2017-12-08 11:25:27 -0800

    added copyright and new digipen logo

--- DarkenedPath(WIP)/DarkenedPath(WIP)/GameStateDigiPenLogo.c
+++ DarkenedPath(WIP)/DarkenedPath(WIP)/GameStateDigiPenLogo.c
@@ -28,25 +28,50 @@
 //----------------------------------------------------------------------------*/
 static const char * DigiPenLogo[] =
 {
-	"                                _______                  ________              ",
-	"             /### /#######     |       \\                |        \\             ",
-	"             /### /#########   |        \\  _          _ |   ___   |            ",
-	"             /### /##########  |    |    ||_|        |_||         |            ",
-	"             /### /########### |    |    | _   ____   _ |    ____/____   _____ ",
-	"             /### /########### |    |    || | /    \\ | ||   |    /    \\ |     \\",
-	"             /### /##########  |    |    || ||  __  || ||   |   |  --  ||     |",
-	"             /### /#########   |        / | ||      || ||   |   |  ____||  |  |",
-	"             /### /#######/    |_______/  |_| \\___  ||_||___|    \\____\\ |__|__|",
-	"    /####### /### ///////                     /     |                          ",
-	"  /######### /###                             \\____/                           ",
-	" /########## /###                                                              ",
-	"/########### /###                                                              ",
-	"/########### /###      _ ___ ___   ___ __  _  __ ___ __ __       _     _  _    ",
-	"//########## /### ||\\||_  | | | | | | |__ | ||__  | |__|  |_||\\|| ||  | || _|_|",
-	" //######### /### || | _| | | | |_| | |__ |_||    | |__|__| || ||_||__|_||_| | ",
-	"  ///####### /###                                                              ",
-	"    ///////  ///                                                               ",
+" ,gggggggggggg,                           ,ggggggggggg,                                 ,a8a,                                                                        ",
+"dP\"\"\"88\"\"\"\"\"\"Y8b,                        dP\"\"\"\88\"\"\"\"\"\"Y8,                              ,8\" \"8,                           I8            I8                            ",
+"Yb,  88       `8b,                       Yb,  88      `8b                              d8   8b                           I8            I8                            ",
+" `\"  88        `8b  gg                gg  `\"  88      ,8P                              88   88                        88888888  gg  88888888                         ",
+"     88         Y8  \"\"                \"\"      88aaaad8P\"                               88   88                           I8     \"\"     I8                            ",
+"     88         d8  gg     ,gggg,gg   gg      88\"\"\"\"\"    ,ggg,    ,ggg,,ggg,           Y8   8P  ,ggg,,ggg,     ,g,       I8     gg     I8    gg      gg   ,ggg,      ",
+"     88        ,8P  88    dP\"  \"Y8I   88      88        i8\" \"8i  ,8\" \"8P\" \"8,          `8, ,8\' ,8\" \"8P\" \"8,   ,8\'8,      I8     88     I8    I8      8I  i8\" \"8i     ",
+"     88       ,8P\'  88   i8\'    ,8I   88      88        I8, ,8I  I8   8I   8I     8888  \"8,8\"  I8   8I   8I  ,8\'  Yb    ,I8,    88    ,I8,   I8,    ,8I  I8, ,8I     ",
+"     88______,dP' _,88,_,d8,   ,d8I _,88,_    88        `YbadP\' ,dP   8I   Yb,    `8b,  ,d8b, ,dP   8I   Yb,,8\'_   8)  ,d88b, _,88,_ ,d88b, ,d8b,  ,d8b, `YbadP\'     ",
+"    888888888P\"   8P\"\"Y8P\"Y8888P\"8888P\"\"Y8    88       888P\"Y8888P\'   8I   `Y8      \"Y88P\" \"Y88P\'   8I   `Y8P\' \"YY8P8P88P\"\"Y888P\"\"Y888P\"\"Y888P\'\"Y88P\"`Y8888P\"Y888    ",
+"                               ,d8I\'                                                                                                                                 ",
+"                             ,dP\'8I                                                                                                                                  ",
+"                            ,8\"  8I                                                                                                                                  ",
+"                            I8   8I                                                                                                                                  ",
+"                            `8, ,8I                                                                                                                                  ",
+"                             `Y8P\"                                                                                                                                   ",
+"                                              ,ggggggggggggggg                                                                                                       ",
+"                                  ,dPYb,     dP\"\"\"\"\"\"\88\"\"\"\"\"\"\"              ,dPYb,                                ,dPYb,                                             ",
+"                                  IP\'`Yb     Yb,_    88                     IP\'`Yb                                IP\'`Yb                                             ",
+"                                  I8  8I      `\"\"    88                     I8  8I                                I8  8I                                             ",
+"                                  I8  8\'             88                     I8  8\'                                I8  8\'                                             ",
+"                       ,ggggg,    I8 dP              88   ,ggg,     ,gggg,  I8 dPgg,    ,ggg,,ggg,     ,ggggg,    I8 dP    ,ggggg,      ,gggg,gg  gg     gg          ",
+"                      dP\"  \"Y8ggg I8dP               88  i8\" \"8i   dP\"  \"Yb I8dP\" \"8I  ,8\" \"8P\" \"8,   dP\"  \"Y8ggg I8dP    dP\"  \"Y8ggg  dP\"  \"Y8I  I8     8I          ",
+"                     i8\'    ,8I   I8P          gg,   88  I8, ,8I  i8\'       I8P    I8  I8   8I   8I  i8'    ,8I   I8P    i8\'    ,8I   i8\'    ,8I  I8,   ,8I          ",
+"                    ,d8,   ,d8\'  ,d8b,_         \"Yb,,8P  `YbadP\' ,d8,_    _,d8     I8,,dP   8I   Yb,,d8,   ,d8\'  ,d8b,_ ,d8,   ,d8\'  ,d8,   ,d8I ,d8b, ,d8I          ",
+"                    P\"Y8888P\"    PI8\"8888         \"Y8P\' 888P\"Y888P\"\"Y8888PP88P     `Y88P\'   8I   `Y8P\"Y8888P\"    8P\'\"Y88P\"Y8888P\"    P\Y8888P\"888P\"\"Y88P\"888         ",
+"                                  I8 `8,                                                                                                    ,d8I\'      ,d8I\'         ",
+"                                  I8  `8,                                                                                                 ,dP\'8I     ,dP\'8I          ",
+"                                  I8   8I                                                                                                ,8\"  8I    ,8\"  8I          ",
+"                                  I8   8I                                                                                                I8   8I    I8   8I          ",
+"                                  I8, ,8'                                                                                                `8, ,8I    `8, ,8I          ",
+"                                   \"Y8P\'                                                                                                  `Y8P\"      `Y8P\"4     ",
 };
+
+
+
+
+
+
+
+
+
+
+
 
 /*------------------------------------------------------------------------------
 // Private Function Declarations:
@@ -56,13 +81,24 @@ static const char * DigiPenLogo[] =
 // Public Functions:
 //----------------------------------------------------------------------------*/
 
+
 void GameStateDigiPenLogoInit()
 {
 	HANDLE  hConsole;
-	int k = 4;
+	int k =5;
 	int j;
 	int w;
 	w = 15;
+
+
+	HWND console = GetConsoleWindow();
+
+	system("mode 165,35");   //Set mode to ensure window does not exceed buffer size
+	SMALL_RECT WinRect = { 0, 0, 165, 35 };   //New dimensions for window in 8x12 pixel chars
+	SMALL_RECT* WinSize = &WinRect;
+	SetConsoleWindowInfo(GetStdHandle(STD_OUTPUT_HANDLE), true, WinSize);   //Set new size for window
+
+	HWND consoleWindow = GetConsoleWindow();
 
 
 	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -80,7 +116,7 @@ void GameStateDigiPenLogoInit()
 	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, w);
 	Wait(500);
-	return 0;
+	return;
 }
 
 void GameStateDigiPenLogoUpdate(float dt)
@@ -88,6 +124,8 @@ void GameStateDigiPenLogoUpdate(float dt)
 	/* Tell the compiler that the 'dt' variable is unused. */
 	UNREFERENCED_PARAMETER(dt);
 
+	/* Print the message "copyright..." to the screen. */
+	printf("Copyright 2017 Digipen (USA) Corporation \n");
 	/* Print the message "Press ENTER to continue..." to the screen. */
 	printf("Press any button to continue...");
 
